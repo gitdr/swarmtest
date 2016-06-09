ef42289e8e08412844d7d5aa8c97dcbf1f49b5c8
Dmitry Risics <dmitrijs.risics@vce.com>
2016-06-09 23:20:28 +0100

    working version

--- base.sh
+++ base.sh
@@ -11,15 +11,19 @@ apt-get  -y -o "DPkg::Options::=--force-confold" -o "DPkg::Options::=--force-con
 
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list
+# echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list
+echo "deb http://ubuntu-mirror.home.net/apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
 
 apt-get  -y update
 apt-get  -y -o "DPkg::Options::=--force-confold" -o "DPkg::Options::=--force-confdef" install linux-image-extra-$(uname -r) docker-engine
 
 usermod -aG docker vagrant
 
+IP=$(ifconfig  | grep -A 1 eth1 | grep -v eth1 | awk '{print $2}' | cut -d ':' -f 2)
+
 if ! grep --quiet "192\.168\.1\.250" /etc/default/docker; then
-  echo 'DOCKER_OPTS="--dns 192.168.1.250 -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --insecure-registry 192.168.1.85:5000"' >> /etc/default/docker
+  echo "DOCKER_OPTS=\"--dns 192.168.1.250 -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --insecure-registry 192.168.1.85:5000 \
+        --cluster-store=consul://10.1.0.2:8500 --cluster-advertise=$IP:4000 --storage-driver=aufs\"" >> /etc/default/docker
   service docker restart
 fi
 
--- swarm1.sh
+++ swarm1.sh
@@ -9,6 +9,6 @@ if ! $( docker ps | grep --quiet "consul" ); then
 fi
 
 if ! $( docker ps | grep --quiet "swarm" ); then
-  IP=$(ifconfig  | grep -A 1 eth1 | grep -v eth1 | awk '{print $2}' | cut -d ':' -f 2)
+  # IP=$(ifconfig  | grep -A 1 eth1 | grep -v eth1 | awk '{print $2}' | cut -d ':' -f 2)
   docker run -d -p 4000:4000 192.168.1.85:5000/swarm manage -H :4000 --replication --advertise $IP:4000 consul://$IP:8500
 fi
